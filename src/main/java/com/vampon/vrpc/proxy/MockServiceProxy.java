28531fcce3730439104dfcc938392d27cef1c325
403500543@qq.com <403500543@qq.com>
2024-04-28 15:58:46 +0800

    Added the function of mock simulation calls

--- src/main/java/com/vampon/vrpc/proxy/MockServiceProxy.java
+++ src/main/java/com/vampon/vrpc/proxy/MockServiceProxy.java
@@ -1,5 +1,38 @@
 package com.vampon.vrpc.proxy;
 
-public class MockServiceProxy {
-    
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class MockServiceProxy implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 根据方法的返回值类型，生成特定的默认值对象
+        Class<?> methodReturnType = method.getReturnType();
+        log.info("mock invoke {}",method.getName());
+        return getDefaultObject(methodReturnType);
+    }
+
+    public Object getDefaultObject(Class<?> type){
+        // 基本数据类型
+        if(type.isPrimitive()){
+            if(type == boolean.class){
+                return false;
+            } else if (type == short.class) {
+                return (short)0;
+            } else if (type == int.class) {
+                return 0;
+            } else if (type == long.class) {
+                return 0L;
+            } else if (type == float.class) {
+                return 0.0;
+            }
+        }
+        // 对象类型
+        // todo:待完善(faker之类的伪造数据生成库？)
+        return null;
+    }
 }
--- src/main/java/com/vampon/vrpc/proxy/ServiceProxyFactory.java
+++ src/main/java/com/vampon/vrpc/proxy/ServiceProxyFactory.java
@@ -1,11 +1,14 @@
 package com.vampon.vrpc.proxy;
 
+import com.vampon.vrpc.RpcApplication;
+
 import java.lang.reflect.Proxy;
 
 /**
  * 服务代理工厂（用于创建代理对象）
  *
  */
+// todo:逻辑可以想想怎么再拆分一下
 public class ServiceProxyFactory {
 
     /**
@@ -16,9 +19,25 @@ public class ServiceProxyFactory {
      * @return
      */
     public static <T> T getProxy(Class<T> serviceClass) {
+        if(RpcApplication.getRpcConfig().isMock()){
+            return getMockProxy(serviceClass);
+        }
         return (T) Proxy.newProxyInstance(
                 serviceClass.getClassLoader(),
                 new Class[]{serviceClass},
                 new ServiceProxy());
+    }
+
+    /**
+     * 根据服务类获取Mock代理对象
+     * @param serviceClass
+     * @return
+     * @param <T>
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass){
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy());
     }
 }
